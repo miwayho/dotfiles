123921eb1979e506d4fd76eba5e4cf3922497f30
miwayho <miwayho@gmail.com>
2025-02-06 21:52:48 +0300

    modified:   config/polybar/modules/window.sh

--- config/polybar/modules/window.sh
+++ config/polybar/modules/window.sh
@@ -1,14 +1,34 @@
 #!/bin/bash
 
 i3-msg -t get_tree | jq -r '
+    def truncate(s):
+        if (s | length) > 20 then (s[0:17] + "...") else s end;
+
     [ 
         .. | select(.nodes? and .type == "workspace") as $ws
         | $ws.nodes[], $ws.floating_nodes[]
         | select(.window_properties? and .window)
-        | { workspace: $ws.num, class: (.window_properties.class | if . == "org.wezfurlong.wezterm" then "terminal" else . end | ascii_downcase) }
+        | { 
+            workspace: $ws.num, 
+            class: (.window_properties.class | ascii_downcase),
+            name: .name
+        }
     ]
-    | group_by(.class)
-    | map({class: .[0].class, workspaces: map(.workspace) | unique | join(",")})
-    | map("\(.workspaces) \(.class)")
+    | map({
+        workspace: .workspace,
+        class: (
+            if .class == "org.wezfurlong.wezterm" then "terminal \(truncate(.name))"
+            elif .class == "telegramdesktop" then "telegram"
+            elif .class == "kicad" then "kicad \(truncate(.name))"
+            else truncate(.class) end
+        )
+    })
+    | group_by(.workspace)
+    | map({
+        workspace: .[0].workspace,
+        windows: (map(.class) | unique | join(","))
+    })
+    | sort_by(.workspace)
+    | map("\(.workspace) \(.windows)")
     | join(" | ")
 '
